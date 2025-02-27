6e94aa12f26baa93ec947988a9bbd33c8d649a6c
Jerel Hope B. Paligumba <jerel.paligumba10@gmail.com>
2025-02-27 11:40:36 +0800

    Restart from Scratch

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,39 +10,39 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        // User::create([
-        //     'name' => 'Admin',
-        //     'email' => 'admin@gmail.com',
-        //     'password' => Hash::make('adminpassword'),
-        //     'role' => 'admin',
-        // ]);
-
-        $cluster = User::create([
-            'name' => 'Sandra (Cluster 4)',
-            'email' => 'sandra.cluster4@gmail.com',
-            'password' => Hash::make('password'),
-            'role' => 'cluster',
-            'cluster_id' => null, // Clusters don’t belong to another cluster
-            'is_active' => true,
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('adminpassword'),
+            'role' => 'admin',
         ]);
 
-        // List of barangays
-        $barangayNames = [
-            '19', '27', '33', 'Pahanocoy', 'Singcang','Tangub',
-            'Pta. Taytay', 'Cabug', 'Sum-ag', 'Felisa', 'Handumanan', 'Alijis', 'Alangilan'
-            , 'Villamonte'
-        ];
+        // $cluster = User::create([
+        //     'name' => 'Sandra (Cluster 4)',
+        //     'email' => 'sandra.cluster4@gmail.com',
+        //     'password' => Hash::make('password'),
+        //     'role' => 'cluster',
+        //     'cluster_id' => null, // Clusters don’t belong to another cluster
+        //     'is_active' => true,
+        // ]);
 
-        // Create barangays under this cluster
-        foreach ($barangayNames as $barangay) {
-            User::create([
-                'name' => "Barangay $barangay",
-                'email' => strtolower("barangay$barangay@gmail.com"),
-                'password' => Hash::make('password'),
-                'role' => 'barangay',
-                'cluster_id' => $cluster->id, // Assign to Cluster 1
-                'is_active' => true,
-            ]);
-        }
+        // // List of barangays
+        // $barangayNames = [
+        //     '19', '27', '33', 'Pahanocoy', 'Singcang','Tangub',
+        //     'Pta. Taytay', 'Cabug', 'Sum-ag', 'Felisa', 'Handumanan', 'Alijis', 'Alangilan'
+        //     , 'Villamonte'
+        // ];
+
+        // // Create barangays under this cluster
+        // foreach ($barangayNames as $barangay) {
+        //     User::create([
+        //         'name' => "Barangay $barangay",
+        //         'email' => strtolower("barangay$barangay@gmail.com"),
+        //         'password' => Hash::make('password'),
+        //         'role' => 'barangay',
+        //         'cluster_id' => $cluster->id, // Assign to Cluster 1
+        //         'is_active' => true,
+        //     ]);
+        // }
     }
 }
